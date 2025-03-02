98d167326750f097bb1cabd98acf037673f16fe8
JTaylor <jtaylor@jfd.co.uk>
2025-03-02 11:16:18 +0000

    build: add footer

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-gray-900 text-white py-8 mt-12">
+    <div class="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        <p class="text-sm">&copy; {{ now()->year }} Your Company. All rights reserved.</p>
+        <nav class="mt-4 md:mt-0">
+
+        </nav>
+    </div>
+</footer>
--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -25,6 +25,9 @@
         @endif
     @endforeach
 
+    <livewire:footer />
+
+
 @livewireScripts
 </body>
 </html>
