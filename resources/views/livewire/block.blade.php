9cded11e305a9560da0b84e0be65b90efbc7ab15
JTaylor <jtaylor@jfd.co.uk>
2025-03-04 16:31:54 +0000

    build: mobile layout

--- resources/views/livewire/block.blade.php
+++ resources/views/livewire/block.blade.php
@@ -1,4 +1,4 @@
-<div class=" [ relative h-screen ]"
+<div class=" [ relative h-screen overflow-hidden ]"
 style="background-color: {{ $block['data']['bg_color'] ?? '' }};">
     <div class="container mx-auto h-full">
         <div class="grid grid-cols-1 md:grid-cols-2 items-center h-full">
@@ -18,14 +18,14 @@ style="background-color: {{ $block['data']['bg_color'] ?? '' }};">
                 @endif
             </div>
             <div class="[ hero__image ] [ flex justify-center ]
-                @if ($block['data']['block_invert']) md:order-1 @else md:order-2 @endif">
+                @if ($block['data']['block_invert']) md:order-1 @else md:order-2 @endif h-full @if ($block['data']['block_layout'] === 'image-boxed') relative @endif">
                 @if ($block['data']['block_image'])
                     <img src="{{ asset('storage/' . $block['data']['block_image']) }}" alt="Block Image"
                     class="
                         max-w-full h-auto object-cover
-                        @if ($block['data']['block_layout'] === 'image-background') absolute inset-0 w-full h-full object-cover opacity-50
-                        @elseif ($block['data']['block_layout'] === 'image-half') absolute top-0 w-1/2 h-full @if ($block['data']['block_invert']) left-0 @else right-0 @endif object-cover
-                        @elseif ($block['data']['block_layout'] === 'image-boxed') max-w-lg mx-auto 
+                        @if ($block['data']['block_layout'] === 'image-background') md:absolute inset-0 w-full h-full object-cover opacity-50 
+                        @elseif ($block['data']['block_layout'] === 'image-half') md:absolute top-0 md:w-1/2 h-full @if ($block['data']['block_invert']) left-0 @else right-0 @endif object-cover 
+                        @elseif ($block['data']['block_layout'] === 'image-boxed') md:absolute max-lg-w top-0 h-full mx-auto 
                         @endif
                     ">
                 @endif
--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -1,30 +1,31 @@
-<div class="[ hero ] [ relative lg:h-screen ]"
-style="background-color: {{ $heroBgColor }};">
-
+<div class=" [ relative h-screen overflow-hidden ]"
+style="background-color: {{ $block['data']['bg_color'] ?? '' }};">
     <div class="container mx-auto h-full">
         <div class="grid grid-cols-1 md:grid-cols-2 items-center h-full">
             <div class="[ hero__copy ] [ z-30 relative text-center md:text-left px-6 ]
-                @if ($heroInvert) md:order-2 @else md:order-1 @endif">
-                @if ($heroTitle)
+                @if ($block['data']['block_invert']) md:order-2 @else md:order-1 @endif">
+                @if ($block['data']['block_title'])
                     <h1 class="text-4xl font-bold"
-                        style="color: {{ $heroAccentColor }}"
-                    >{{ $heroTitle }}</h1>
+                        style="color: {{ $block['data']['accent_color'] ?? '' }};"
+                    >{{ $block['data']['block_title'] }}</h1>
                 @endif
-                @if ($heroSubtitle)
+                @if ($block['data']['block_description'])
                     <p class="text-lg mt-4"
-                        style="color: @if ($heroTextColor == 'dark') {{ $dark_color }} @elseif ($heroTextColor == 'light') {{ $light_color }} @endif"
-                    >{{ $heroSubtitle }}</p>
+                        style="color: @if (isset($block['data']['text_color']) && $block['data']['text_color'] == 'dark') {{ $dark_color }} 
+                            @elseif (isset($block['data']['text_color']) && $block['data']['text_color'] == 'light') {{ $light_color }} 
+                            @endif"
+                    >{{ $block['data']['block_description'] }}</p>
                 @endif
             </div>
-            <div class="[ hero__image ] [ flex justify-center ]
-                @if ($heroInvert) md:order-1 @else md:order-2 @endif">
-                @if ($heroImage)
-                    <img src="{{ asset('storage/' . $heroImage) }}" alt="Hero Image"
+            <div class="[ flex justify-center ]
+                @if ($block['data']['block_invert']) md:order-1 @else md:order-2 @endif h-full @if ($block['data']['block_layout'] === 'image-boxed') relative @endif">
+                @if ($block['data']['block_image'])
+                    <img src="{{ asset('storage/' . $block['data']['block_image']) }}" alt="Block Image"
                     class="
                         max-w-full h-auto object-cover
-                        @if ($heroLayout === 'image-background') absolute inset-0 w-full h-full object-cover opacity-50
-                        @elseif ($heroLayout === 'image-half') absolute top-0 w-1/2 h-full @if ($heroInvert) left-0 @else right-0 @endif object-cover
-                        @elseif ($heroLayout === 'image-boxed') max-w-lg mx-auto 
+                        @if ($block['data']['block_layout'] === 'image-background') md:absolute inset-0 w-full h-full object-cover opacity-50 
+                        @elseif ($block['data']['block_layout'] === 'image-half') md:absolute top-0 md:w-1/2 h-full @if ($block['data']['block_invert']) left-0 @else right-0 @endif object-cover 
+                        @elseif ($block['data']['block_layout'] === 'image-boxed') md:absolute max-lg-w top-0 h-full mx-auto 
                         @endif
                     ">
                 @endif
