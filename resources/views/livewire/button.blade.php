93cdb3f6036918e80209fc4da0f6237098383906
JTaylor <jtaylor@jfd.co.uk>
2025-03-07 16:11:23 +0000

    build: button cleanup

--- resources/views/livewire/button.blade.php
+++ resources/views/livewire/button.blade.php
@@ -1,23 +1,9 @@
-<a href="{{ $url }}"
->
-    <div class="mt-3 p-3 w-fit hover:opacity-50"
-    style="
-      @if ( $dark_mode )
-          background-color: {{ $light_color }};
-          color: {{ $dark_color }};
-      @else
-          background-color: {{ $dark_color }};
-          color: {{ $light_color }};
-      @endif
-      @if ( $bg_color )
-          background-color: {{ $bg_color }};
-      @endif
-      @if ( $text_color == 'dark' )
-        color: {{ $dark_color }};
-      @elseif ( $text_color == 'light' )
-        color: {{ $light_color }};
-      @endif
-   ">
+<a href="{{ $url }}" class="block mt-3 w-fit hover:opacity-50">
+    <div class="p-3"
+         style="
+            background-color: {{ $bg_color ?: ($dark_mode ? $light_color : $dark_color) }};
+            color: {{ $text_color == 'dark' ? $dark_color : ($text_color == 'light' ? $light_color : ($dark_mode ? $dark_color : $light_color)) }};
+         ">
         {{ $title }}
     </div>
 </a>
--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,9 +1,7 @@
 <footer class="" style="
     padding-top: calc((54vh - 100vw )/ -10); padding-bottom: calc((56vh - 100vw )/ -10);">
-    <div class="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+    <div class="container mx-auto px-4">
         <p class="text-sm">&copy; {{ now()->year }} {{ $site_name }}. All rights reserved.</p>
-        <nav class="mt-4 md:mt-0">
-
-        </nav>
+ 
     </div>
 </footer>
